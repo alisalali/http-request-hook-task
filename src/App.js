10af4c051bae02345bacaa64dc1f8a6876eca6f7
alisalali <alis.alali@hotmail.com>
2023-12-07 17:09:08 +0300

    adjust callback function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -6,19 +6,26 @@ import useHttp from "./Hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const transformTask = useCallback((taskObj) => {
-    const loadedTasks = [];
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-    setTasks(loadedTasks);
-  }, []);
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTask);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks({
-      url: "https://tsts-15dc1-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-    });
+    const transformTask = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://tsts-15dc1-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      },
+      transformTask
+    );
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
