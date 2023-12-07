70cdbed2339196d0acb58856a14d8876b4e2c289
alisalali <alis.alali@hotmail.com>
2023-12-07 16:10:28 +0300

    useHttp custom hook

--- src/App.js
+++ src/App.js
@@ -2,38 +2,29 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./Hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://tsts-15dc1-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
+  const transformTask = (taskObj) => {
+    const loadedTasks = [];
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
+    setTasks(loadedTasks);
   };
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://tsts-15dc1-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+    },
+    transformTask
+  );
 
   useEffect(() => {
     fetchTasks();
--- src/Hooks/use-http.js
+++ src/Hooks/use-http.js
@@ -9,9 +9,9 @@ const useHttp = (requestConfig, applyData) => {
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method,
-        headers: requestConfig.headers,
-        body: JSON.stringify(requestConfig.body),
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
@@ -19,22 +19,13 @@ const useHttp = (requestConfig, applyData) => {
       }
 
       const data = await response.json();
-
       applyData(data);
 
-      // const loadedTasks = [];
-
-      // for (const taskKey in data) {
-      //   loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      // }
-
-      // setTasks(loadedTasks);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
   };
-
   return {
     isLoading,
     error,
