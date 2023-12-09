9e51ff58fbf0f21770ff4a47229c986d6f1d3d3d
alisalali <alis.alali@hotmail.com>
2023-12-09 21:40:11 +0300

    create task hook with bind

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
-import useHttp from "./Hooks/use-http";
+import useHttp from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
